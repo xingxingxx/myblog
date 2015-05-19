47860adf1b68d71f74cdc1d1f25b3335fe1104cd
肖兴平 <xx9815@qq.com>
2015-05-19 17:14:07 +0800

    开始开发话题功能

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    /* 模板相关配置 */
+    'TMPL_PARSE_STRING' => array(
+        '__IMG__'    => __ROOT__ . '/_assets/img',
+        '__CSS__'    => __ROOT__ . '/_assets/css',
+        '__JS__'     => __ROOT__ . '/_assets/js',
+    ),
 );
