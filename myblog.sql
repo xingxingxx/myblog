96d7a6cdf4555aa6c5c9fb4b4ef041ce90378233
肖兴平 <xx9815@qq.com>
2015-05-28 18:45:40 +0800

    数据库user表

--- myblog.sql
+++ myblog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015-05-27 13:03:48
+-- Generation Time: 2015-05-28 12:44:56
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -80,36 +80,36 @@ CREATE TABLE IF NOT EXISTS `bbs_topic` (
 
 CREATE TABLE IF NOT EXISTS `bbs_user` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
+  `username` varchar(30) NOT NULL,
   `password` char(32) NOT NULL,
   `email` varchar(50) NOT NULL,
   `sex` bit(1) NOT NULL DEFAULT b'0',
   `sign` varchar(255) NOT NULL,
-  `regtime` int(11) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `regip` char(15) NOT NULL,
+  `reg_time` int(11) NOT NULL,
+  `last_login_time` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `reg_ip` bigint(20) NOT NULL,
+  `last_login_ip` bigint(20) NOT NULL,
+  `login` int(11) NOT NULL DEFAULT '0',
   `token` char(32) NOT NULL,
-  `token_exptime` int(11) NOT NULL,
   `avatar` char(35) NOT NULL,
   `birthday` int(11) NOT NULL,
   `qq` char(10) NOT NULL,
-  `tel` char(11) NOT NULL,
+  `mobile` char(11) NOT NULL,
   `province` int(11) NOT NULL,
   `city` int(11) NOT NULL,
   `district` int(11) NOT NULL,
   `community` int(11) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `address` varchar(80) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=36 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
 
 --
 -- 转存表中的数据 `bbs_user`
 --
 
-INSERT INTO `bbs_user` (`uid`, `username`, `password`, `email`, `sex`, `sign`, `regtime`, `status`, `regip`, `token`, `token_exptime`, `avatar`, `birthday`, `qq`, `tel`, `province`, `city`, `district`, `community`, `address`) VALUES
-(33, '123456', '96e79218965eb72c92a549dd5a330112', 'xx9815@qq.com', b'0', '', 1432724031, 0, '2130706433', '41dcce8e42e2c81ea4abb7ec74a28b90', 1432810431, '', 0, '', '', 0, 0, 0, 0, ''),
-(34, '1234567', '96e79218965eb72c92a549dd5a330112', 'xiao.xingping@163.com', b'0', '', 1432724066, 0, '2130706433', '248df3132d5b8cb34bfef0a784894b13', 1432810466, '', 0, '', '', 0, 0, 0, 0, ''),
-(35, 'admin', '7fa8282ad93047a4d6fe6111c93b308a', 'xx9815@qq.com1', b'0', '', 1432724129, 0, '2130706433', '5980141cd4bcdec614d21222cfa38e1b', 1432810529, '', 0, '', '', 0, 0, 0, 0, '');
+INSERT INTO `bbs_user` (`uid`, `username`, `password`, `email`, `sex`, `sign`, `reg_time`, `last_login_time`, `status`, `reg_ip`, `last_login_ip`, `login`, `token`, `avatar`, `birthday`, `qq`, `mobile`, `province`, `city`, `district`, `community`, `address`) VALUES
+(38, 'xingxingxx', '6d465b01bcfb39bc2bd64d7e92bafafa', 'xx9815@qq.com', b'0', '', 1432805079, 1432808262, 1, 2130706433, 2130706433, 6, 'bfe5f7dca520c2822e54fc7731a6b7b0', '', 0, '', '', 0, 0, 0, 0, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
