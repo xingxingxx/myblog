73531eea66f07ef391923e2179d15d52ba61ad8d
肖兴平 <xx9815@qq.com>
2015-06-01 18:39:51 +0800

    优化代码结构

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
     //模板相关配置
     'TMPL_PARSE_STRING' => array(
-        '__IMG__'    => __ROOT__ . '/_assets/img',
-        '__CSS__'    => __ROOT__ . '/_assets/css',
-        '__JS__'     => __ROOT__ . '/_assets/js',
+        '__IMG__'    => __ROOT__ . '/Public/img',
+        '__CSS__'    => __ROOT__ . '/Public/css',
+        '__JS__'     => __ROOT__ . '/Public/js',
     ),
     //邮件配置
     'THINK_EMAIL' => array(
