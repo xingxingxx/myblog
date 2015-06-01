b2b60c7c67e86595929800d591c95863ea75ba80
肖兴平 <xx9815@qq.com>
2015-06-01 19:12:26 +0800

    登录过滤

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -24,6 +24,10 @@ class UserController extends HomeController
         $this->User = D('User');
     }
 
+    protected function _initialize(){
+        is_login() || $this->error('您还没有登录，请先登录！');
+    }
+
     /**
      * 首页
      */
